d5996c0ed6a658f0e828b74ecda4fedb615a7655
Rasim <eferiums@yandex.ru>
2024-09-10 02:38:07 +0400

    feat: реализован header

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import styles from './Index.module.scss';
+
+import Header from '../Header/Header';
+
+const Main = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+};
+
+export default Main;
