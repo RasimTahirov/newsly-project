5033925e7bcf6834cdc735117ecc32b497d7a958
Rasim <eferiums@yandex.ru>
2024-10-11 15:22:02 +0400

    fix: исправлена навигация

--- src/app/routes/AppRoutesNav.tsx
+++ src/app/routes/AppRoutesNav.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import News from '../../pages/News/News';
 
 const AppRoutesNav = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/news" replace />} />
       <Route path="/news" element={<News />} />
     </Routes>
   );
